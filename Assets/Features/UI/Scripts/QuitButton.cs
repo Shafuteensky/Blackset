a638f006be74add6571fb29e146340a2c8a905b0
Ildar <ildaravgn@gmail.com>
2026-02-04 12:32:46 +0600

    Верстка главного меню и настроек

---
+++ Assets/Features/UI/Scripts/QuitButton.cs
@@ -0,0 +1,20 @@
+using Extensions.Generics;
+using UnityEditor;
+using UnityEngine;
+
+namespace Blackset.UI
+{
+    /// <summary>
+    /// Кнопка выхода из игры
+    /// </summary>
+    public class QuitButton : GenericButton
+    {
+        public override void OnButtonClick()
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
