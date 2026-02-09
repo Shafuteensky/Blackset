0f8e770e962bb583a47d596a6a45bba43159cd5f
Ildar <ildaravgn@gmail.com>
2026-02-09 17:09:40 +0600

    Идентифицируемый объект Extensiond.Identification

--- Assets/Plugins/Extensions/Identification/ID.cs
+++ Assets/Plugins/Extensions/Identification/ID.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Extensions.Identification
@@ -7,24 +6,5 @@ namespace Extensions.Identification
     /// Универсальный идентификатор
     /// </summary>
     [CreateAssetMenu(menuName = "Extensions/" + nameof(ID))]
-    public class ID : ScriptableObject
-    {
-        protected const string GUID_FORMAT = "N";
-        
-        /// <summary>
-        /// Идентификатор
-        /// </summary>
-        public string Id => id;
-        
-        [SerializeField]
-        protected string id = string.Empty;
-        
-        private void OnValidate()
-        {
-            if (string.IsNullOrEmpty(id))
-            {
-                id = Guid.NewGuid().ToString(GUID_FORMAT);
-            }
-        }
-    }
+    public class ID : IdentifiableObject { }
 }
---
+++ Assets/Plugins/Extensions/Identification/IdentifiableObject.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Extensions.Identification
+{
+    /// <summary>
+    /// Базовый идентифицруемый скриптовый объект
+    /// </summary>
+    public abstract class IdentifiableObject : ScriptableObject
+    {
+        protected const string GUID_FORMAT = "N";
+        
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public string Id => id;
+        
+        [SerializeField]
+        protected string id = string.Empty;
+        
+        protected virtual void OnValidate()
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                id = Guid.NewGuid().ToString(GUID_FORMAT);
+            }
+        }
+    }
+}
