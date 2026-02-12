3dc636619c97458446cda928b022a56fe96d516c
Ildar <ildaravgn@gmail.com>
2026-02-12 22:09:25 +0600

    Тестовые изменение и вывод данных игрока

--- Assets/Features/PlayerMetaData/Scripts/PlayerDataTest.cs
+++ Assets/Features/PlayerMetaData/Scripts/PlayerDataTest.cs
@@ -24,43 +24,55 @@ namespace Blackset.Player
         [SerializeField]
         [Min(0)]
         protected int money = 1;
-        
-        private void OnEnable()
+
+        [ContextMenu("Add Random Dice")]
+        public void AddRandomDice()
         {
-            // Получение данных
+            IReadOnlyList<DiceData> diceData = gameDataRegistry.Dices.Data;
+            List<DiceItemCell> dicesInventoryData = playerDataFacade.DicesInventory.Data;
+            
+            int randomIndex = Random.Range(0, diceData.Count);
+            DiceData randomDice = diceData[randomIndex];
+            randomIndex = Random.Range(0, randomDice.AvailableTypes.Count);
+            DiceType randomDiceType = randomDice.AvailableTypes[randomIndex];
+            
+            playerDataFacade.DicesInventory.AddItem(new DiceItemCell(randomDice.Id, randomDiceType.Id));
+            
+            ServiceDebug.Log($"Добавлен новый дайс: {randomDice.DataName}, {randomDiceType.DataName}. " +
+                             $"\nТеперь в инвентаре {dicesInventoryData.Count} дайсов: {GetDicesInventoryList("    ")}");
+        }
+
+        [ContextMenu("Add 10 Money")]
+        public void AddSomeMoney() => AddMoney();
+        public void AddMoney(int moneyToAdd = 10)
+        {
+            PlayerMetaData metaData = playerDataFacade.MetaData.Data;
+            
+            metaData.AddMoney(moneyToAdd);
+            ServiceDebug.Log($"Добавлена валюта ({money}, в сумме {metaData.Money})");
+        }
+
+        [ContextMenu("Print All Player Data")]
+        public void PrintPlayerData()
+        {
             PlayerMetaData metaData = playerDataFacade.MetaData.Data;
             List<DiceItemCell> dicesInventoryData = playerDataFacade.DicesInventory.Data;
             
-            // Вывод данных игрока до обновления
-            ServiceDebug.Log($"Текущие данные игрока:" +
-                             $"\n{metaData.Money}$" +
-                             $"\n{dicesInventoryData.Count} дайсов: {GetDicesInventoryList()}");
-            
-            // Обновление мета-данных и инвентарей игрока
-            // Деньги
-            metaData.AddMoney(money);
-            // Случайный дайс
-            IReadOnlyList<DiceData> diceData = gameDataRegistry.Dices.Data;
-            int randomIndex = Random.Range(0, diceData.Count);
-            DiceData randomDice = diceData[randomIndex];
-            randomIndex = Random.Range(0, diceData.Count);
-            DiceType randomDiceType = gameDataRegistry.DiceTypes.Data[randomIndex];
-            playerDataFacade.DicesInventory.Add(new DiceItemCell(randomDice.Id, randomDiceType.Id));
-            
-            // Вывод данных игрока после обновления
-            ServiceDebug.Log($"Данные игрока после обновления:" +
-                             $"\n{metaData.Money}$" +
-                             $"\n{dicesInventoryData.Count} дайсов: {GetDicesInventoryList()}");
+            ServiceDebug.Log($"Данные игрока:" +
+                             $"\nВалюта: {metaData.Money}$" +
+                             $"\nИнвантарь:" +
+                             $"\n   - Дайсы ({dicesInventoryData.Count} в сумме): {GetDicesInventoryList("        ")}");
         }
 
-        private string GetDicesInventoryList()
+        private string GetDicesInventoryList(string prefix = "")
         {
             string dicesInInventory = String.Empty;
             foreach (DiceItemCell cell in playerDataFacade.DicesInventory.Data)
             {
                 DiceData diceInCell = cell.GetItemData(gameDataRegistry.Dices);
+                if (diceInCell == null) continue;
                 DiceType diceType = cell.GetTypeData(gameDataRegistry.DiceTypes);
-                dicesInInventory += $"{diceInCell.DataName} [{diceType.DataName}], ";
+                dicesInInventory += $"\n{prefix}- {diceInCell.DataName}, {diceType.DataName} ({cell.ItemAmount} шт)";
             }
 
             return dicesInInventory;
